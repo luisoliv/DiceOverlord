2c447197e94312f20b273a7486a162e6d4f18802
luisoliv <luisdoom1995@hotmail.com>
2018-01-09 17:58:34 -0400

    changed the way results are returned
    changed the way of results returned, so now intead of returning an entire already parsed string, it returns an integer array in the form of [result1, result2, .... , total] and then i parse it as a final string result in a separate function (better code practice)

--- diceBot.js
+++ diceBot.js
@@ -7,15 +7,15 @@ function print(text)
 	console.log(text);
 }
 
-function executeCommand(cmd)
+function executeRoll(cmd)
 {
-	var result = "The rolls are: {";
+	var result = []; //the array that will contain final results
 	var total = 0;
 	var diceRoll = 0;
 	
 	cmd = cmd.split('d');
 	
-	quantity = cmd[0].split("#");
+	quantity = cmd[0].split('#');
 	quantity = parseInt(quantity[1]);
 
 	size = parseInt(cmd[1]);
@@ -28,17 +28,40 @@ function executeCommand(cmd)
 	for(var i = 0; i < quantity; i++)
 	{
 		diceRoll = Math.floor( (Math.random() * size) + 1);
-		print("roll "+i+": "+diceRoll);
-		result += diceRoll;
-		
-		if(i < quantity - 1)
-			result += ",";
+		result.push(diceRoll);
+
 		total += diceRoll;
 	}
-	
-	result += "} = " + total;
+	result.push(total);
 	
 	return result;
+}
+
+/*
+	function used to debug the roll results since it changed the way it returns it
+	@define arrResult array with all roll results, and the cumulative total as last member of array
+*/
+function logResult(arrResult)
+{
+	for(var i = 0; i < (arrResult.length - 1) ; i++)
+	{
+		console.log('\n' + i + '° ' + '= ' + arrResult[i])
+	}
+	console.log('\n\nTotal : ' + arrResult[ arrResult.length - 1]);
+}
+
+function parseResults(arrResult)
+{
+	var final = '(';
+	for(var i = 0; i < (arrResult.length - 1) ; i++)
+	{
+		final += arrResult[i]; //implicit convertion from INT to STRING
+		if( (i + 1) < (arrResult.length - 1))
+			final += ', ';
+	}
+	final += ') = [' + arrResult[ arrResult.length - 1] + ']';
+
+	return final;
 }
 
 function connectBot(name,pass,network,chan,port)
@@ -54,14 +77,15 @@ function connectBot(name,pass,network,chan,port)
     	print(from + '@' + to + ' : ' + message);
     	if(message[0] == '#')
     	{
-    		var result = executeCommand(message);
+    		let results = executeRoll(message);
+    		//logResult(result);
     		
-    		if(result)
+    		if(results)
     		{
-    			client.say(chan, result);
+    			client.say(chan, parseResults(results) );
     		}
     		else
-    			client.say(chan,"hey " + t + ", those are too many dices! (max. 25 dices)");
+    			client.say(chan,'hey ' + t + ', those are too many dices! (max. 25 dices)');
     	}
 	});
 }
@@ -71,13 +95,13 @@ main();
 
 function main()
 {
-	var nickName = "DiceOverlord";
-	var password = diceBot;
-	var network = "irc.freenode.net";
-	var channel = "##uac";   //here you can change it for your channel
+	var nickName = 'DiceOverlord';
+	var password = 'diceBot';
+	var network = 'irc.freenode.net';
+	var channel = '##uac';   //here you can change it for your channel
 	var port = 6999;
 	
-	print("Welcome to the IRC Dice bot, made by luisoliv");
+	print('Welcome to the IRC Dice bot, made by luisoliv');
 	connectBot(nickName,password,network,channel,port);
 	
 	return true;
